809e702293f542fcf682b9e9fe20ba1bc8d7d7cf
ire4564 <ire4564@gmail.com>
2025-07-18 22:03:06 +0900

    Add error handling for undefined NOTION_PAGE_ID and improve getPosts function

--- src/libs/utils/notion/filterPosts.ts
+++ src/libs/utils/notion/filterPosts.ts
@@ -28,13 +28,13 @@ export function filterPosts(
     })
     // filter status
     .filter((post) => {
-      const postStatus = post.status[0]
-      return acceptStatus.includes(postStatus)
+      const postStatus = post.status?.[0]
+      return postStatus && acceptStatus.includes(postStatus)
     })
     // filter type
     .filter((post) => {
-      const postType = post.type[0]
-      return acceptType.includes(postType)
+      const postType = post.type?.[0]
+      return postType && acceptType.includes(postType)
     })
   return filteredPosts
 }
